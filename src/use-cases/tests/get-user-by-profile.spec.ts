5c6a93d4397ec34007f9ec38f7f3a6be175acc22
Jose Lima <jalimag0206@gmail.com>
2023-12-08 03:31:49 -0300

    test: ensure can be able to get user profile

---
+++ src/use-cases/tests/get-user-by-profile.spec.ts
@@ -0,0 +1,36 @@
+import {
+    InMemoryCreateUserRepository,
+    InMemoryGetUserByIdRepository,
+} from '@/repositories/in-memory/in-memory-users-repository'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { GetUserProfileUseCase } from '../get-user-profile'
+import { hash } from 'bcryptjs'
+import { ResourceNotFoundError } from '@/errors/resource-not-found'
+
+let createUserRepository: InMemoryCreateUserRepository
+let getUserByIdRepository: InMemoryGetUserByIdRepository
+let sut: GetUserProfileUseCase
+
+describe('Get User Profile Use Case', () => {
+    beforeEach(() => {
+        createUserRepository = new InMemoryCreateUserRepository()
+        getUserByIdRepository = new InMemoryGetUserByIdRepository(
+            createUserRepository
+        )
+        sut = new GetUserProfileUseCase(getUserByIdRepository)
+    })
+
+    it('should be able to get user profile', async () => {
+        const createdUser = await createUserRepository.execute({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password_hash: await hash('123456', 6),
+        })
+
+        const { user } = await sut.execute({
+            userId: createdUser.id,
+        })
+
+        expect(user.name).toEqual('John Doe')
+    })
+})
