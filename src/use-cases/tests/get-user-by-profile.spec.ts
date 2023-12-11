438f3333bc436e154daf15fe325af4ca48fc93a3
Jose Lima <jalimag0206@gmail.com>
2023-12-11 18:38:16 -0300

    fix: ensure expect is await function

--- src/use-cases/tests/get-user-by-profile.spec.ts
+++ src/use-cases/tests/get-user-by-profile.spec.ts
@@ -35,7 +35,7 @@ describe('Get User Profile Use Case', () => {
     })
 
     it('should not be able to get user profile with wrong id', async () => {
-        expect(() =>
+        await expect(() =>
             sut.execute({
                 userId: 'not-existing-id',
             })
