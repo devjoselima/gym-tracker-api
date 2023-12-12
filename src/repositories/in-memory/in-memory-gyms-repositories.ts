978ab56280b3b8ae2fad00ff27e3bc4cedd7e4fc
Jose Lima <jalimag0206@gmail.com>
2023-12-12 18:13:09 -0300

    feat: add in memory create gym repository

--- src/repositories/in-memory/in-memory-gyms-repositories.ts
+++ src/repositories/in-memory/in-memory-gyms-repositories.ts
@@ -1,8 +1,30 @@
-import { Gym } from '@prisma/client'
-import { IGetGymById } from '../interfaces'
+import { Gym, Prisma } from '@prisma/client'
+import { IGetGymById, ICreateGymRepository } from '../interfaces'
+import { randomUUID } from 'crypto'
 
+export class InMemoryCreateGymRepository implements ICreateGymRepository {
+    public items: Gym[] = []
+    async execute(data: Prisma.GymCreateInput) {
+        const gym = {
+            id: randomUUID(),
+            title: data.title,
+            description: data.description ?? null,
+            phone: data.phone ?? null,
+            latitude: new Prisma.Decimal(data.latitude.toString()),
+            longitude: new Prisma.Decimal(data.longitude.toString()),
+            created_at: new Date(),
+        }
+
+        this.items.push(gym)
+
+        return gym
+    }
+}
 export class InMemoryGetGymById implements IGetGymById {
     public items: Gym[] = []
+    constructor(private createGymRepository: InMemoryCreateGymRepository) {
+        this.items = createGymRepository.items
+    }
     async execute(id: String) {
         const gym = this.items.find((item) => item.id === id)
 
--- src/repositories/interfaces/gym.ts
+++ src/repositories/interfaces/gym.ts
@@ -1,5 +1,8 @@
-import { Gym } from '@prisma/client'
+import { Gym, Prisma } from '@prisma/client'
 
+export interface ICreateGymRepository {
+    execute(data: Prisma.GymCreateInput): Promise<Gym>
+}
 export interface IGetGymById {
     execute(id: string): Promise<Gym | null>
 }
