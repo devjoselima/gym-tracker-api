be6ba5805a29c19f29a09800fb0b8d1b066ccc00
Jose Lima <jalimag0206@gmail.com>
2023-12-13 21:07:29 -0300

    test: ensure user be able to fetch check-ins by user id

---
+++ src/use-cases/tests/fetch-user-check-ins-history.spec.ts
@@ -0,0 +1,42 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { FetchUserCheckInsHistoryUseCase } from '../fetch-user-check-ins-history'
+import {
+    InMemoryCreateCheckInsRepository,
+    InMemoryFindManyCheckInsByUserId,
+} from '@/repositories/in-memory/in-memory-checkins-repository'
+
+let createCheckInRepository: InMemoryCreateCheckInsRepository
+let findManyCheckInsByUserId: InMemoryFindManyCheckInsByUserId
+let sut: FetchUserCheckInsHistoryUseCase
+
+describe('Find many check ins by user ID', () => {
+    beforeEach(async () => {
+        createCheckInRepository = new InMemoryCreateCheckInsRepository()
+        findManyCheckInsByUserId = new InMemoryFindManyCheckInsByUserId(
+            createCheckInRepository
+        )
+        sut = new FetchUserCheckInsHistoryUseCase(findManyCheckInsByUserId)
+    })
+
+    it('should be able to fetch check-in repository', async () => {
+        await createCheckInRepository.execute({
+            gym_id: 'gym-01',
+            user_id: 'user-01',
+        })
+
+        await createCheckInRepository.execute({
+            gym_id: 'gym-02',
+            user_id: 'user-01',
+        })
+
+        const { checkIns } = await sut.execute({
+            userId: 'user-01',
+        })
+
+        expect(checkIns).toHaveLength(2)
+        expect(checkIns).toEqual([
+            expect.objectContaining({ gym_id: 'gym-01' }),
+            expect.objectContaining({ gym_id: 'gym-02' }),
+        ])
+    })
+})
