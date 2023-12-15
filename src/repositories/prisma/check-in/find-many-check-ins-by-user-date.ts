10c3897e96061d849f8f337006974974305c6c32
Jose Lima <jalimag0206@gmail.com>
2023-12-15 18:21:38 -0300

    feat: add FindManyCheckInsByUserDateRepository

---
+++ src/repositories/prisma/check-in/find-many-check-ins-by-user-date.ts
@@ -0,0 +1,24 @@
+import { prisma } from '@/lib/prisma'
+import { IGetCheckInByUserDateRepository } from '@/repositories/interfaces'
+import dayjs from 'dayjs'
+
+export class FindManyCheckInsByUserDateRepository
+    implements IGetCheckInByUserDateRepository
+{
+    async execute(userId: string, date: Date) {
+        const startOfTheDay = dayjs(date).startOf('date')
+        const endOfTheDay = dayjs(date).endOf('date')
+
+        const checkIn = await prisma.checkIn.findFirst({
+            where: {
+                user_id: userId,
+                created_at: {
+                    gte: startOfTheDay.toDate(),
+                    lte: endOfTheDay.toDate(),
+                },
+            },
+        })
+
+        return checkIn
+    }
+}
