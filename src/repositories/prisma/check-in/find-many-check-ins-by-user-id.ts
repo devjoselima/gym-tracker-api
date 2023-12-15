482bcd030b432dc518d2816b0e36b0ce8bdd4c05
Jose Lima <jalimag0206@gmail.com>
2023-12-15 18:09:03 -0300

    feat: add FindManyCheckInsByIdRepository

---
+++ src/repositories/prisma/check-in/find-many-check-ins-by-user-id.ts
@@ -0,0 +1,18 @@
+import { prisma } from '@/lib/prisma'
+import { IFindManyCheckInsByUserIdRepository } from '@/repositories/interfaces'
+
+export class FindManyCheckInsByIdRepository
+    implements IFindManyCheckInsByUserIdRepository
+{
+    async execute(userId: string, page: number) {
+        const checkIns = await prisma.checkIn.findMany({
+            where: {
+                user_id: userId,
+            },
+            skip: (page - 1) * 20,
+            take: 20,
+        })
+
+        return checkIns
+    }
+}
