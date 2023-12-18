38421f080659250e03d1fd7dfa016b9a74fb1365
Jose Lima <jalimag0206@gmail.com>
2023-12-18 18:29:25 -0300

    fix: fix repository name

--- src/repositories/prisma/gym/get-gym-by-id.ts
+++ src/repositories/prisma/gym/get-gym-by-id.ts
@@ -1,7 +1,7 @@
 import { prisma } from '@/lib/prisma'
 import { IGetGymByIdRepository } from '@/repositories/interfaces'
 
-export class PrismaGetUserByIdRepository implements IGetGymByIdRepository {
+export class PrismaGetGymByIdRepository implements IGetGymByIdRepository {
     async execute(id: string) {
         const gym = await prisma.gym.findUnique({
             where: {
