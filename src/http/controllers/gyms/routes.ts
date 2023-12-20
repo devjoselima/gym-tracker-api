00f4d5b7597850990768a67e03edcaa2d7e37672
Jose Lima <jalimag0206@gmail.com>
2023-12-20 19:25:22 -0300

    feat: add gyms routes

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -2,6 +2,17 @@ import { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
+import {
+    createGymController,
+    nearbyGymsController,
+    searchGymsController,
+} from './'
+
 export const gymsRoutes = async (app: FastifyInstance) => {
     app.addHook('onRequest', verifyJWT)
+
+    app.get('/gyms/search', searchGymsController)
+    app.get('/gyms/nearby', nearbyGymsController)
+
+    app.post('/gyms', createGymController)
 }
