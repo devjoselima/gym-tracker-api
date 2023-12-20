5dc0d791fe639e28f80239f03260a6ca7410f327
Jose Lima <jalimag0206@gmail.com>
2023-12-20 18:52:23 -0300

    test: add create user test e2e

--- src/http/controllers/test/create-user.spec.ts
+++ src/http/controllers/test/create-user.spec.ts
@@ -1,3 +1,23 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
 
-test('ok', () => {})
+describe('Create user (e2e)', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('should be able to create user', async () => {
+        const response = await request(app.server).post('/users').send({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '1234567',
+        })
+
+        expect(response.statusCode).toEqual(201)
+    })
+})
