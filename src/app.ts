2dd4031a7eba82d9c307d72305a439d71241a833
Jose Lima <jalimag0206@gmail.com>
2023-12-23 03:24:17 -0300

    feat: add refresh token

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import fastify from 'fastify'
+import fastifyCookie from '@fastify/cookie'
 import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
@@ -9,8 +10,17 @@ import { checkInsRoutes } from './http/controllers/check-ins/routes'
 
 export const app = fastify()
 
+app.register(fastifyCookie)
+
 app.register(fastifyJwt, {
     secret: env.JWT_SECRET,
+    cookie: {
+        cookieName: 'refreshToken',
+        signed: false,
+    },
+    sign: {
+        expiresIn: '10m',
+    },
 })
 
 app.register(usersRoutes)
--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -32,9 +32,27 @@ export const authenticateController = async (
             }
         )
 
-        return reply.status(200).send({
-            token,
-        })
+        const refreshToken = await reply.jwtSign(
+            {},
+            {
+                sign: {
+                    sub: user.id,
+                    expiresIn: '7d',
+                },
+            }
+        )
+
+        return reply
+            .setCookie('refreshToken', refreshToken, {
+                path: '/',
+                secure: true,
+                sameSite: true,
+                httpOnly: true,
+            })
+            .status(200)
+            .send({
+                token,
+            })
     } catch (error) {
         if (error instanceof InvalidCredentialsError) {
             return reply.status(400).send({
